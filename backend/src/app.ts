a33d081933de1828975a5c9b21dad716dbf2df8f
rogeriootto <rogerio.ocupado@gmail.com>
2024-08-26 16:21:50 -0300

    feat: aceita cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,19 +1,27 @@
-import express from 'express';
-
-import authRoutes from './routes/authRoutes';
-import userRoutes from './routes/userRoutes';
-import registrationRoutes from './routes/registrationRoutes';
-import symposiumRoutes from './routes/symposiumRoutes';
-import eventRoutes from './routes/eventRoutes';
+import express from "express";
+import cors from "cors";
+import authRoutes from "./routes/authRoutes";
+import userRoutes from "./routes/userRoutes";
+import registrationRoutes from "./routes/registrationRoutes";
+import symposiumRoutes from "./routes/symposiumRoutes";
+import eventRoutes from "./routes/eventRoutes";
 // Importe outras rotas conforme necessário
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
 // Middleware global
 app.use((req, res, next) => {
-    next();
+  next();
 });
 
 // Rotas
@@ -26,8 +34,8 @@ app.use(eventRoutes);
 
 // Error handling
 app.use((err: any, req: any, res: any, next: any) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'An error occurred on the server' });
+  console.error(err.stack);
+  res.status(500).json({ message: "An error occurred on the server" });
 });
 
 export { app };
